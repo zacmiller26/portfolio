cb44b26e284d116b6da3d44f60509fce8ebe3ef2
Zac (mac-mini) <zacmiller26@icloud.com>
2021-10-10 09:06:25 -0600

    add site icons + meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="robots" content="noindex" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, maximum-scale=1.0, user-scalable=0, initial-scale=1"
+        />
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#1e232f" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="nofollow, noindex" />
+        <link rel="mask-icon" href='/favicon.svg' color="#2bb7ff" />
+        <link rel="icon" href='/favicon.png' />
+        <link rel="shortcut icon" href='/favicon.png' />
+        <link rel="apple-touch-icon" href='/favicon.png' />
+        <link rel="apple-touch-icon-precomposed" href='/favicon.png' />
+        <meta property="og:site_name" content="Web Portfolio" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
