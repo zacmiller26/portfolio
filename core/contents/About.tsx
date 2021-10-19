c1d2e8962f2c2753839ae4d8ee7fbc4154b16936
Zac (mac-mini) <zacmiller26@icloud.com>
2021-10-19 09:52:26 -0600

    improve About component, fix mobile overflow

--- core/contents/About.tsx
+++ core/contents/About.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import Image from 'next/image'
 
 import styles from './About.module.sass'
@@ -46,39 +46,109 @@ const Tag: React.FC<Props> = (props) => (
 const AboutMeText = () => (
   <>
     <p>
-      I'm a full-stack web developer, and I love the process of taking
-      a <Tag>new</Tag> concept and forging it into <Tag>reality.</Tag>
+      I love the full-stack process of taking
+      a new concept and forging it into reality.
+      Especially the front-end: It's all about the UI/UX being seamless, 
+      intuitive and purposeful.
     </p>
-    <p>In the last ten years, <Tag>my projects</Tag> have seen some
-    neat metrics, like <Tag>1.1 billion <em>page views</em>,</Tag> 
-    <Tag>70 million <em>unique users</em>,</Tag> and 
-    <Tag>3.5 million <em>unique monthly users</em>.</Tag>
+    <p>In the last ten years, <Tag>my projects</Tag> have seen 
+    metrics like <Tag>1.1 billion</Tag> <em>page views</em>,
+    <Tag>70 million</Tag> <em>unique users</em>, and 
+    <Tag>3.5 million</Tag> <em>unique monthly users</em>.
     </p>
     <p>
-      I've learned a lot through my <Tag>self-employed</Tag> endeavors, 
-      and am looking  to continue learning and growing in a <Tag>team</Tag> 
-      {' '}environment. Let's connect!
+      I've learned a lot through my self-employed endeavors, 
+      and am looking  to continue learning and growing in a team
+      environment. Let's connect!
     </p>
   </>
 )
 
+const PICTURE_GALLERY = [
+  'hanoi.png',
+  'portland.png',
+  'cycling.png',
+  'prerace.png',
+  'racing.png',
+  'podium.png'
+]
+
 const ProfilePicture = () => {
 
+  const [image, setImage] = useState(0)
+
+  const changeImage = useCallback((index?: number) => {
+    if(index !== undefined) setImage(index)
+    else {
+      setImage(prev => {
+        if(prev < PICTURE_GALLERY.length - 1) {
+          return prev + 1
+        }
+        return 0
+      })
+    }
+  }, [])
+
+  const getPercentage = (whole: number, part: number, dec?: number) => (
+    whole === 0 ? 0 : parseFloat((100 * (part / whole)).toFixed(dec ? dec : 2))
+  )
+
+  useEffect(() => {
+    const amount = 10000
+    const timeout = setTimeout(() => {
+      changeImage()
+    }, amount)
+    return () => clearTimeout(timeout)
+  }, [image])
+
+
   return (
-    <div className={styles.profilePicture}>
+    <>
+      <div className={styles.profilePicture}>
 
-      <span className={styles.corner1} />
-      <span className={styles.corner2} />
+        <span className={styles.corner1} />
+        <span className={styles.corner2} />
 
-      <i className={styles.image}>
-        <Image 
-          src="/images/thumbnail.png" 
-          width={226}
-          height={226}
-        />
-      </i>
+        <div className={styles.imagesWrapper}>
+          <div 
+            className={styles.imagesContainer}
+            style={{
+              width: PICTURE_GALLERY.length * 100 + '%',
+              left: '-' + ((image) * 100) + '%'
+            }}
+          >
+            {PICTURE_GALLERY.map((fileName, index) => (
+              <i 
+                className={styles.image} 
+                key={fileName} 
+                data-active={index === image}
+              >
+                <Image 
+                  src={`/images/profile/${fileName}`}
+                  width={250}
+                  height={250}
+                  quality={100}
+                />
+            </i>
+            ))}
+          </div>
+        </div>
 
-    </div>
+
+      </div>
+      <div className={styles.galleryTracker}>
+        {PICTURE_GALLERY.map((fileName, index) => (
+          <button 
+            key={fileName}
+            className={styles.galleryBtn}
+            onClick={( ) => changeImage(index)}
+            type="button"
+            data-active={index === image}
+          >
+          </button>
+        ))}
+      </div>
+    </>
   )
 }
 
